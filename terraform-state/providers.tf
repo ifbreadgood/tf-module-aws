73b4a113ffa3a50fc94a6b11b6e154ac7fa1ff9d
ifbread <ifbread@pm.me>
2025-06-02 08:19:06 -0400

    add option for assume role

--- terraform-state/providers.tf
+++ terraform-state/providers.tf
@@ -6,16 +6,28 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = "us-east-2"
+
   default_tags {
-    tags = merge(var.tags, {location: "us-east-2"})
+    tags = merge(var.tags, { location : "us-east-2" })
+  }
+
+  assume_role {
+    role_arn = var.provider_assume_role_arn
   }
 }
+
 provider "aws" {
   region = "us-east-1"
   alias  = "backup"
+
   default_tags {
-    tags = merge(var.tags, {location: "us-east-1"})
+    tags = merge(var.tags, { location : "us-east-1" })
+  }
+
+  assume_role {
+    role_arn = var.provider_assume_role_arn
   }
 }
--- terraform-state/variables.tf
+++ terraform-state/variables.tf
@@ -4,3 +4,7 @@ variable "name" {
 variable "tags" {
   type = map(string)
 }
+variable "provider_assume_role_arn" {
+  type    = string
+  default = null
+}
